41407ec520e766706f0d93ec4c55499c9ffd8775
Ulrich Kautz <ulrich.kautz@gmail.com>
2018-06-12 17:02:43 +0200

    Update opsgenie-open-tab.js

--- opsgenie-open-tab.js
+++ opsgenie-open-tab.js
@@ -3,12 +3,13 @@
 // @version      0.1
 // @description  Revert the UI fail of using JS instead of native A tags, which disables ctrl+click and midle mouse button to open new tabs
 // @author       Me
-// @match        https://app.opsgenie.com/alert/V2
+// @match        https://app.opsgenie.com/alert*
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
+    console.log("OpsGenie improvements online");
 
     var $ = jQuery;
 
@@ -20,7 +21,7 @@
         var found = 0;
         items.not('.x-mmb-monkeypatch-applied').each(function() {
             if ($(this).hasClass('x-mmb-monkeypatch-applied')) {
-                console.log("Bad apple");
+                console.log("OpsGenie improvements: Bad apple");
                 return;
             }
             found++;
@@ -30,22 +31,28 @@
                     var counter = $(this).find('.item-counter');
                     var id = $.trim(counter.text()).replace(/[^0-9]/, '');
                     var url = 'https://opsg.in/i/'+ id;
-                    window.open(url, '_blank');
+                    if (ev.shiftKey) {
+                        alert(url);
+                    } else {
+                        window.open(url, '_blank');
+                    }
                     return false;
                 }
             });
         });
-        if (found) console.log("Applied MMB patch to", found, "items");
+        if (found) console.log("OpsGenie improvements: Applied MMB patch to", found, "items");
     }
 
     // tech so far solid, intercom button just annoys
     function clearIntercom() {
-        $('#intercom-container-body,#intercom-container').remove();
+        $('#intercom-container, #intercom-frame').remove();
     }
 
     console.log("Loading MMB patch");
+    for (var i = 1; i <= 10; i++) {
+        setTimeout(clearIntercom, 100 * i);
+    }
     setInterval(bindItemsToMMB, 1000);
-    setInterval(clearIntercom, 1000);
 
     // Your code here...
 })();
